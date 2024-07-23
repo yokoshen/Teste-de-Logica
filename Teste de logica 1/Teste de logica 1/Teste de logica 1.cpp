2be281f946c5a8c667bd3e5ba15ff3887adb59cb
yokoshen <30274434+yokoshen@users.noreply.github.com>
2024-07-22 21:40:39 -0300

    Update Teste de logica 1.cpp

--- Teste de logica 1/Teste de logica 1/Teste de logica 1.cpp
+++ Teste de logica 1/Teste de logica 1/Teste de logica 1.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include "Windows.h"
 
+/*
+Dado o Array de inteiros abaixo, ordene-o de tal forma que os números “1” estejam à esquerda. 
+Os itens devem ser modificados no lugar, ou seja, você não ira trocar posições e sim colocar os números “1” no inicio do Array.
+[2, 1, 5, 2, 5, 2, 1, 1, 1, 7, 9, 13, 127, 21]
+*/
+
 #define MAX_ARRAY 14
 #define NULLO 999 
 using namespace std;
